042d2c1e9f7004d72be0f3353ce8099fbde7403a
Yashraj Balidani <ybalidani19.yr@gmail.com>
2020-05-14 17:32:06 +0530

    Update ATM.java

--- ATM/ATM.java
+++ ATM/ATM.java
@@ -1,1 +1,10 @@
+import java.io.IOException;
+public class ATM extends OptionMenu{
 
+  public static void main(String[] args) throws IOException {
+    OptionMenu optionMenu= new OptionMenu();
+
+    optionMenu.getLogin();
+  }
+
+};
