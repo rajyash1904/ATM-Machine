d097f3127ad1aa56fdedb1d6be4b2b61899e50e6
elkinsd96 <70239254+elkinsd96@users.noreply.github.com>
2021-07-06 15:06:57 -0600

    Project rework

--- ATM/ATM.java
+++ ATM/ATM.java
@@ -1,6 +1,6 @@
 import java.io.IOException;
 
-public class ATM extends OptionMenu {
+public class ATM {
 
 	public static void main(String[] args) throws IOException {
 		OptionMenu optionMenu = new OptionMenu();
