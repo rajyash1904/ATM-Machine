8b80a3a20fe99e03aa83ffef72d53516bac8d908
elkinsd96 <70239254+elkinsd96@users.noreply.github.com>
2021-07-06 15:02:22 -0600

    Project rework

--- ATM/ATM.java
+++ ATM/ATM.java
@@ -1,10 +1,14 @@
 import java.io.IOException;
-public class ATM extends OptionMenu{
 
-  public static void main(String[] args) throws IOException {
-    OptionMenu optionMenu= new OptionMenu();
+public class ATM extends OptionMenu {
 
-    optionMenu.getLogin();
-  }
+	public static void main(String[] args) throws IOException {
+		OptionMenu optionMenu = new OptionMenu();
+		introduction();
+		optionMenu.mainMenu();
+	}
 
-};
+	public static void introduction() {
+		System.out.println("Welcome to the ATM Project!");
+	}
+}
